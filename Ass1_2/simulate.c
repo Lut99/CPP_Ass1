ed8619c5895873bfd84fa8336e0ea9afc3f8d215
Lut99 <mullertim99@gmail.com>
2019-11-10 17:02:35 +0100

    Added OPENMP implementation
    Co-authored-by: MarkMeltzer <m.kirkeng@gmail.com>

--- Ass1_2/simulate.c
+++ Ass1_2/simulate.c
@@ -6,8 +6,15 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <omp.h>
 
 #include "simulate.h"
+
+
+/* Add any global variables you may need. */
+const double c = 0.15;
+
+/* Add any functions you may need (like a worker) here. */
 
 
 /*
@@ -17,7 +24,7 @@
  *
  * i_max: how many data points are on a single wave
  * t_max: how many iterations the simulation should run
- * num_threads: how many threads to use
+ * num_threads: how many threads to use (excluding the main threads)
  * old_array: array of size i_max filled with data for t-1
  * current_array: array of size i_max filled with data for t
  * next_array: array of size i_max. You should fill this with t+1
@@ -25,9 +32,25 @@
 double *simulate(const int i_max, const int t_max, const int num_threads,
         double *old_array, double *current_array, double *next_array)
 {
-    /*
-     * Your implementation should go here.
-     */
+    /* Simulate a fucking wave */
+    int t, i;
+    double *temp;
 
+    omp_set_num_threads(num_threads);
+
+    for (t = 0; t < t_max; t++) {
+        #pragma omp parallel for
+        for (i = 1; i < i_max - 1; i++) {
+            next_array[i] = 2 * current_array[i] - old_array[i] + c * 
+                            (current_array[i - 1] - 
+                            (2 * current_array[i] - current_array[i + 1]));
+        }
+        temp = old_array;
+        old_array = current_array;
+        current_array = next_array;
+        next_array = temp;
+    }
+
+    /* You should return a pointer to the array with the final results. */
     return current_array;
 }
