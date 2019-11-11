6401bc140cf179e0d0063a1fd34ecf8d103a22e7
MarkMeltzer <m.kirkeng@gmail.com>
2019-11-11 14:59:50 +0100

    Functional sequential sieve implementation.

--- Ass1_3/sieve.c
+++ Ass1_3/sieve.c
@@ -1,31 +1,50 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include "timer.h"
 
-int main(int argc, int *argv[]){
-    long primes[100];
+int main(int argc, char *argv[]){
+    timer_start();
+
+    // number of natural numbers
+    int n = 30000;
+
+    // init list of primes
+    long primes[n];
     long primelen = 1;
     primes[0] = 2;
 
-    int nat = 2;
     // iterate over natural numbers
-    while (1 == 1){
+    long nat = 2;
+    while (true){
+        bool is_prime = true;
+        
         // iterate of primes
         for (int i = 0; i < primelen; i++){
             // check if number is devisible by prime
             if (nat % primes[i] == 0) {
-                is_prime = 0;
+                is_prime = false;
                 break;
             }
-            is_prime = 1;
         }
 
+        // add prime to list of primes
+        if (is_prime == true){
+            primes[primelen] = nat;
+            primelen++;
 
-        if (is_prime = 1){
-            
+            printf("new prime: %ld\n", nat);
         }
         
+        // stop the program early
+        if (primelen == n){
+            double time = timer_end();
+            printf("Took %g seconds\n", time);
+            printf("Normalized: %g seconds\n", time / (1. * primelen));
+            exit(1);
+        }
 
-
-
-
+        // get the next natural number
+        nat++;
     }
 }
