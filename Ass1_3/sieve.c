c6327f52ab16df56e4858811be1970f2420fa24b
MarkMeltzer <m.kirkeng@gmail.com>
2019-11-11 14:25:07 +0100

    Added files for 1.3

---
+++ Ass1_3/sieve.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(int argc, int *argv[]){
+    long primes[100];
+    long primelen = 1;
+    primes[0] = 2;
+
+    int nat = 2;
+    // iterate over natural numbers
+    while (1 == 1){
+        // iterate of primes
+        for (int i = 0; i < primelen; i++){
+            // check if number is devisible by prime
+            if (nat % primes[i] == 0) {
+                is_prime = 0;
+                break;
+            }
+            is_prime = 1;
+        }
+
+
+        if (is_prime = 1){
+            
+        }
+        
+
+
+
+
+    }
+}
